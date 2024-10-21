441d4cfc212cf1a0ac3663a0a0d15f360438b1c4
OOPS! Studio! <gh1@seattle1.com>
2024-10-21 01:08:56 -0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { browser } from '$app/environment';
+
+	let text = "hello";
+
+	if(browser){
+                console.log("running");
+		text = "goodbye";
+	}
+</script>
+
+<div>{@html text}</div>
